93a938bbecdbcfcf7a55fa3c9f7281617698c886
sugrobov.m <ml.sugrobov@gmail.com>
2022-08-19 14:46:45 +0300

    added dockerfile

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+import json
+import mysql.connector
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -40,6 +42,52 @@ def create():
             return 'Что-то пошло не так, проверьте правильность заполняемых данных'
     else:
         return render_template('create.html')
+
+
+@app.route('/pictures')
+def get_widgets():
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        password="password",
+        database="inventory"
+    )
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM widgets")
+    row_headers = [x[0] for x in cursor.description]
+    results = cursor.fetchall()
+    json_data = []
+    for result in results:
+        json_data.append(dict(zip(row_headers, result)))
+    cursor.close()
+    return json.dumps(json_data)
+
+
+@app.route('/initdb')
+def db_init():
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        password="p@ssw0rd1"
+    )
+    cursor = mydb.cursor()
+
+    cursor.execute("DROP DATABASE IF EXISTS inventory")
+    cursor.execute("CREATE DATABASE inventory")
+    cursor.close()
+
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        password="password",
+        database="inventory"
+    )
+    cursor = mydb.cursor()
+
+    cursor.execute("DROP TABLE IF EXISTS pictures")
+    cursor.execute("CREATE TABLE pictures (title VARCHAR(255), price INTEGER, active BOOLEAN, description VARCHAR(255))")
+    cursor.close()
+    return 'init database'
 
 
 if __name__ == '__main__':
